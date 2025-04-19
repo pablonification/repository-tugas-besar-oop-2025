751e23b6993d77babe363016fadc7a9f5230d399
Arqila <18223047@std.stei.itb.ac.id>
2025-04-19 15:40:38 +0700

    feat: add project structure for development

---
+++ src/main/java/com/spakborhills/Main.java
@@ -0,0 +1,13 @@
+package com.spakborhills;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting Spakbor Hills game...");
+        // TODO: Initialize game components and start the game
+    }
+} 
